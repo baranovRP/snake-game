00cea5d53b2cfc6fa7e075bd3a009e05636dcd39
baranov.r.p <baranov.r.p@gmail.com>
2021-06-03 12:45:15 +0200

    Track High Score

--- food.py
+++ food.py
@@ -8,6 +8,7 @@ class Food(Turtle):
         super().__init__()
         self.shape()
         self.penup()
+        self.shape("circle")
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
